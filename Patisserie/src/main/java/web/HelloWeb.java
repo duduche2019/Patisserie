e43ef745f8d61227ec196462d727c4949d2a4d9a
niklas <anenico2007@gmail.com>
2019-03-28 12:02:47 +0100

    ajout html

--- Patisserie/src/main/java/web/HelloWeb.java
+++ Patisserie/src/main/java/web/HelloWeb.java
@@ -1,5 +1,6 @@
 package web;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,8 +15,12 @@ public class HelloWeb extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-				
-		resp.getWriter().println("Hello");
+		
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		writer.println("<h1>Boulangerie Flo</h1>");
+		writer.println("<a href='http://www.google.com'>lien de ma boulangerie</a>");
+		
 	}
 	
 	
